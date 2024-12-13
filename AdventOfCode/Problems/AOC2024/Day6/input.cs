50573bcfa486c9099e609e27d7f6717db93750c1
Hookguin <Hookguin@gmail.com>
2024-12-12 19:14:34 -0500

    day 6

---
+++ AdventOfCode/Problems/AOC2024/Day6/input.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Problems.AOC2024.Day6
+{
+    internal class input
+    {
+    }
+}
